d4c10c083321a0bbba01eb3f60bf77224afe2309
2018009143 <jhcse5189@gmail.com>
2020-01-12 19:02:29 +0900

    add. login logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
 
-    function 
+    $("#btnSendCode").click(function() {
+
+        var email = $("#email").val();
+        // if nothing, 
+        if (!email) {
+            // add html contents
+            $("#confirmStatus").html("Invalid Email format:X");
+            // set frame's color to red
+            $("#statusFrame").removeClass("getGrey");
+            $("#statusFrame").addClass("getRed");
+        } else {
+            $("#email").attr('disabled', '');
+            $("#btnSendCode").attr('disabled', '');
+            $("#confirmStatus").html("Go your email '" + email + "' and get authentication numbers:)");
+            // set frame's color to grey
+            $("#statusFrame").removeClass("getRed");
+            $("#statusFrame").addClass("getGrey");
+        }
+        $("#statusFrame").slideToggle("slow");
+        $("#statusFrame").delay(5000).fadeOut(1500);
+    });
+
+    $("#btnCheckCode").click(function() {
+
+        if(!$("#email").val()) {
+            $("#confirmStatus").html("Invalid Email format:X");
+            // set frame's color to red
+            $("#statusFrame").removeClass("getGrey");
+            $("#statusFrame").addClass("getRed");
+        } else {
+            $("#confirmCode").attr('disabled', '');
+            $("#btnCheckCode").attr('disabled', '');
+            $("#confirmStatus").html("In Veryfying...:)");
+            // set frame's color to grey
+            $("#statusFrame").removeClass("getRed");
+            $("#statusFrame").addClass("getGrey");
+        }
+        $("#statusFrame").slideToggle("slow");
+        $("#statusFrame").delay(5000).fadeOut(3000);
+    });
 });
